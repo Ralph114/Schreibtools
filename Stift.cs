d0a55c13e730b9b1c722b18ac921ffb1be780fd6
Ralph114 <72544929+Ralph114@users.noreply.github.com>
2020-11-12 12:48:04 +0100

    Add files via upload

---
+++ Stift.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schreibtools
+{
+    abstract class Stift
+    {
+        public double Strichstaerke { get; set; }
+        public ConsoleColor Farbe { get; set; }
+        public string Typ { get; set; }
+        public Stift(double strichstaerke)
+        {
+            Strichstaerke = strichstaerke;
+            Typ = "unbekannt";
+            Farbe = ConsoleColor.Black;
+        }
+        public Stift(double strichstaerke, ConsoleColor farbe) : this(strichstaerke)
+        {
+            Farbe = farbe;
+
+        }
+        public virtual void SchreibeText(string text)
+        {
+            Console.ForegroundColor = Farbe;
+            Console.WriteLine($"{Typ}, {Strichstaerke}mm,{Farbe}: {text}");
+        }
+        
+    }
+}
